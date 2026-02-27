f10c726f06dfd8ea8351881bea1369fb2db313fe
Kush Goswami <kushvgoswami84@gmail.com>
2026-02-26 22:49:44 -0500

    Read single row from file

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "utils/row.h"
 
 void write_row(FILE *fptr, Row *row);
+void read_file();
 
 int main() {
     FILE *fptr;
@@ -31,12 +32,25 @@ int main() {
 
     fclose(fptr);
 
+    read_file();
+
     printf("Success writing to file!\n");
 
     return 0;
 }
 
 void write_row(FILE *fptr, Row *row) {
-    fseek(fptr, 0L, SEEK_END);
     fwrite(row, sizeof(Row), 1, fptr);
+}
+
+void read_file() {
+    FILE *fptr = fopen("data.bin", "rb");
+   
+    Row new_row;
+
+    fread(&new_row, sizeof(Row), 1, fptr);
+
+    printf("Row id: %d\n", new_row.id);
+    printf("Row name: %s\n", new_row.name);
+    printf("Row age: %d\n", new_row.age);
 }
